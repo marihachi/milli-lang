6f31fbee21bfecc0c98477d008720a264fd3d54d
marihachi <marihachi0620@gmail.com>
2023-10-27 00:29:08 +0900

    vm

--- src/vm/runner.ts
+++ src/vm/runner.ts
@@ -1,37 +1,97 @@
 import { AsmNode } from '../asm/node.js';
 
 export function run(code: AsmNode[]) {
+	const memory: Map<string, { value: number }> = new Map();
+	const stack: (string | number)[] = [];
 	for (const op of code) {
 		switch (op.kind) {
 			case 'Push': {
-				console.log('push', op.value);
+				stack.push(op.value);
 				break;
 			}
 			case 'PushIdent': {
-				console.log('push-ident', op.identifier);
+				stack.push(op.identifier);
 				break;
 			}
-			case 'Add':
-			case 'Sub':
-			case 'Mul':
+			case 'Add': {
+				const a = stack.pop();
+				const b = stack.pop();
+				if (typeof a !== 'number' || typeof b !== 'number') {
+					throw new Error('runtime error. (op: Add)');
+				}
+				const x = a + b;
+				stack.push(x);
+				break;
+			}
+			case 'Sub': {
+				const a = stack.pop();
+				const b = stack.pop();
+				if (typeof a !== 'number' || typeof b !== 'number') {
+					throw new Error('runtime error. (op: Sub)');
+				}
+				const x = a - b;
+				stack.push(x);
+				break;
+			}
+			case 'Mul': {
+				const a = stack.pop();
+				const b = stack.pop();
+				if (typeof a !== 'number' || typeof b !== 'number') {
+					throw new Error('runtime error. (op: Mul)');
+				}
+				const x = a * b;
+				stack.push(x);
+				break;
+			}
 			case 'Div': {
-				console.log(op.kind.toLowerCase());
+				const a = stack.pop();
+				const b = stack.pop();
+				if (typeof a !== 'number' || typeof b !== 'number') {
+					throw new Error('runtime error. (op: Div)');
+				}
+				const x = a / b;
+				stack.push(x);
 				break;
 			}
 			case 'Neg': {
-				console.log('neg');
+				const a = stack.pop();
+				if (typeof a !== 'number') {
+					throw new Error('runtime error. (op: Neg)');
+				}
+				const x = -a;
+				stack.push(x);
 				break;
 			}
 			case 'Store': {
-				console.log('store');
+				const a = stack.pop();
+				if (typeof a !== 'string') {
+					throw new Error('runtime error. (op: Store)');
+				}
+				const b = stack.pop();
+				if (typeof b !== 'number') {
+					throw new Error('runtime error. (op: Store)');
+				}
+				memory.set(a, { value: b });
 				break;
 			}
 			case 'Load': {
-				console.log('load');
+				const a = stack.pop();
+				if (typeof a !== 'string') {
+					throw new Error('runtime error. (op: Load)');
+				}
+				const x = memory.get(a);
+				if (x == null) {
+					throw new Error('runtime error. (op: Load)');
+				}
+				stack.push(x.value);
 				break;
 			}
 			case 'Print': {
-				console.log('print');
+				const a = stack.pop();
+				if (typeof a !== 'number') {
+					throw new Error('runtime error. (op: Print)');
+				}
+				console.log(a);
 				break;
 			}
 		}
