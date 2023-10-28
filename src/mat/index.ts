7fdae2c9317114367bef16e7c211000554878804
marihachi <marihachi0620@gmail.com>
2023-10-28 14:50:16 +0900

    fix mat

--- src/mat/index.ts
+++ src/mat/index.ts
@@ -1,19 +1,13 @@
 import { AsmOperation } from '../asm/operation.js';
-import { AsmWriter } from '../asm/writer.js';
 import { emitAsm } from './asm-emitter.js';
-import { check } from './checker.js';
 import { parse } from './parser.js';
 
 export function assemble(source: string): AsmOperation[] {
 	// parse text asm
 	const tree = parse(source);
 
-	// check semantics
-	check(tree);
-
 	// generate asm code
-	const w = new AsmWriter();
-	const asm = emitAsm(w, tree);
+	const asm = emitAsm(tree);
 
 	return asm;
 }
